75e6256c3a5df9b29e2f1a02ba00e64090a25cac
Jeison Ferrer <jeison120120@hotmail.com>
2021-10-02 19:26:17 -0500

    Animaciones para los botones

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ let nav_icon = document.getElementById('nav-btn');
 let nav =document.getElementsByClassName('nav');
 let header = document.getElementsByTagName('header');
 let redes =document.getElementsByClassName('sobre-mi--redes');
+let btnContactame=document.getElementsByClassName('btn--normal'); 
 
 function navMostrarOcultar(){
     nav[0].classList.toggle("nav-open");
     header[0].classList.toggle('header-open');
     nav_icon.classList.toggle('btn-open');
     redes[0].classList.toggle("redes-open");
+    btnContactame[0].classList.toggle('ocultar');
 }
